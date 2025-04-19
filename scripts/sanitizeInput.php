43d64debb1d33d2b1ac9634ef1647ee13d1a7ecd
Praneeth-Repo <160804512+Praneeth-Repo@users.noreply.github.com>
2025-04-19 12:21:47 -0500

    Delete scripts directory

--- scripts/sanitizeInput.php
+++
@@ -1,9 +0,0 @@
-<?php
-// sanitizeInput.php
-function sanitizeInput($data) {
-    $data = trim($data); // Remove extra spaces
-    $data = stripslashes($data); // Remove backslashes
-    $data = htmlspecialchars($data); // Convert special characters to HTML entities
-    return $data;
-}
-?>
